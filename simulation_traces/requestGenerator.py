004a68ce7e2ec413642d5029f26f2562811be5df
etyl <etyl.sani@gmail.com>
2024-05-05 23:03:26 +0200

    commit

---
+++ simulation_traces/requestGenerator.py
@@ -0,0 +1,18 @@
+
+
+
+apps = []
+
+with f as open("data/service.json"):
+    pass
+
+print("INIT SERVICE DATA")
+
+def getRequests(time : int) -> list[int]:
+    """
+    Params:
+        time : int : time of simulation
+    Returns:
+        requests : list[int] : array of size M corresponding to the number of requests per service
+    """
+    return []
--- simulation_traces/simulationEnv.py
+++ simulation_traces/simulationEnv.py
@@ -19,19 +19,49 @@ from torchrl.envs import (
 from torchrl.envs.transforms.transforms import _apply_to_composite
 from torchrl.envs.utils import check_env_specs, step_mdp
 
-from simulation import Simulation, Data
+from requestGenerator import getRequests
+
+DEFAULT_X = np.pi
+DEFAULT_Y = 1.0
+
+
 
 
 def _step(tensordict):
+    N = tensordict["params","N"]
+    M = tensordict["params","M"]
 
+    time = tensordict["time"]
+
+    action = tensordict["action"]
+    
+    requests = getRequests(time)
+    
+    th, thdot = tensordict["th"], tensordict["thdot"]  # th := theta
+
+    g_force = tensordict["params", "g"]
+    mass = tensordict["params", "m"]
+    length = tensordict["params", "l"]
+    dt = tensordict["params", "dt"]
     u = tensordict["action"].squeeze(-1)
+    u = u.clamp(-tensordict["params", "max_torque"], tensordict["params", "max_torque"])
+    costs = angle_normalize(th) ** 2 + 0.1 * thdot**2 + 0.001 * (u**2)
 
-    reward, data = simulation.tick(u)
-
-    done = False
+    new_thdot = (
+        thdot
+        + (3 * g_force / (2 * length) * th.sin() + 3.0 / (mass * length**2) * u) * dt
+    )
+    new_thdot = new_thdot.clamp(
+        -tensordict["params", "max_speed"], tensordict["params", "max_speed"]
+    )
+    new_th = th + new_thdot * dt
+    reward = -costs.view(*tensordict.shape, 1)
+    done = torch.zeros_like(reward, dtype=torch.bool)
     out = TensorDict(
         {
-            "data": data,
+            "th": new_th,
+            "thdot": new_thdot,
+            "params": tensordict["params"],
             "reward": reward,
             "done": done,
         },
